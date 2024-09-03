5f9f77e69d407e818240d0f749435275a34115bd
Huy-DNA <huydo862003@gmail.com>
2024-09-03 16:43:08 +0700

    chore: use vite

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import dts from "vite-plugin-dts"
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'index',
+      fileName: 'index',
+      formats: ['cjs', 'es'],
+    },
+  },
+  plugins: [
+    dts(),
+  ]
+});
